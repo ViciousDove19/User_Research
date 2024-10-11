4953a1e0cc9097a2b206b7b311d5c1a417a5a26b
ViciousDove19 <rautchinmay19@gmail.com>
2024-10-11 19:02:25 +0530

    last try

--- interview-analyzer/main.py
+++ interview-analyzer/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",  # Local development
-        "https://{your-app-name}.vercel.app"  # Replace with your Vercel URL
+        "https://userreseearchather.netlify.app/"  # Replace with your Vercel URL
     ],  # Allow your Next.js frontend
     allow_credentials=True,
     allow_methods=["*"],
